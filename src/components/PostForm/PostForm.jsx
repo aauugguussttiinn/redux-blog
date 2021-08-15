2adeaad76e87312a005f0988e0f85db6a33e3b9c
Augustin <augustin.berne.pro@gmail.com>
2021-08-15 18:38:27 +0200

    Form submission works 09

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,5 +1,6 @@
+import { addPost } from "actions/post.action";
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const PostForm = () => {
   
@@ -7,11 +8,27 @@ const PostForm = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const user = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    if (title && content) {
+      const data = {
+        title,
+        content,
+        author: user[0].pseudo,
+        likes: 0,
+      };
+
+      dispatch(addPost(data));
+    }
+  }
   
   return (
     
     <div className="form-container">
-      <form >
+      <form onSubmit={ (e) => handleForm(e) }>
         <input type="text" placeholder="Titre du poste" value={ title } onChange={ (e) => setTitle(e.target.value) } />
         <textarea placeholder="Postez vos pensées..." value={ content } onChange={ (e) => setContent(e.target.value) }></textarea>
         <input type="submit" value="Envoyer" />
