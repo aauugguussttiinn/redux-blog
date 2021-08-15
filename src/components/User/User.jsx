10fcc71c0fac1362016937fc4f029439f8e8d2d7
Augustin <augustin.berne.pro@gmail.com>
2021-08-15 18:19:15 +0200

    Likes are ok 56

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { isEmpty } from "components/Utils/Utils";
 
 const User = () => {
 
@@ -8,10 +9,10 @@ const User = () => {
   return (
     <div className="user-container">
       <div className="user">
-        <h3>{ user[0].pseudo }</h3>
+        <h3>{ !isEmpty(user[0]) && user[0].pseudo }</h3>
         <img src="https://thispersondoesnotexist.com/image" alt="" />
         <p>Age : 35 ans</p>
-        <p>Like(s) : 0</p>
+        <p>Like{ !isEmpty(user[0]) && user[0].likes > 1 ? "s" : null} : {!isEmpty(user[0]) && user[0].likes}</p>
       </div>
     </div>
   );
